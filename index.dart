9529b6b07bae13d6f5687c442a2b0cb460416a7b
linjh <516308181@qq.com>
2023-04-14 10:45:21 +0800

    2. maps

--- index.dart
+++ index.dart
@@ -1,9 +1,23 @@
 void main() {
-  var list = <String>[];
-  var list2 = List<String>.filled(4, 'Rosie ALan');
-  list.add('1');
-  list.add('2');
-  list.add('three');
-  print(list);
-  print(list2);
+  // var list = <String>[];
+  // var list2 = List<String>.filled(4, 'Rosie ALan');
+  // list.add('1');
+  // list.add('2');
+  // list.add('three');
+  // print(list);
+  // print(list2);
+  // --------------------------------------------------------------
+  //2. Maps
+  //2.1 第一种定义Maps
+  // var person = {
+  //   "name": 'mozai',
+  //   "age": 18,
+  //   "works": ['敲代码', '刺溜刺溜']
+  // };
+  // print(person['name']);
+  // print(person['works']);
+  //2.2 第二种定义Maps
+  // var person = new Map();
+  // person['work'] = ['敲代码', '刺溜刺溜'];
+  // print(person['work']);
 }
