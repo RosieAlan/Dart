b02ad58f5cbba2ab729ef097e811b0c6c2ae1589
linjh <516308181@qq.com>
2023-04-14 10:39:37 +0800

    01-init

---
+++ index.dart
@@ -0,0 +1,9 @@
+void main() {
+  var list = <String>[];
+  var list2 = List<String>.filled(4, 'Rosie ALan');
+  list.add('1');
+  list.add('2');
+  list.add('three');
+  print(list);
+  print(list2);
+}
